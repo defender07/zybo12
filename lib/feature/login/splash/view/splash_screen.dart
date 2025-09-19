ac94cc09e8a92af0678f0623312fcd8f818f090d
defender07 <njrrohith052@gmail.com>
2025-09-19 14:58:53 +0530

    Added fade-in animation and refactored SplashScreen

--- lib/feature/login/splash/view/splash_screen.dart
+++ lib/feature/login/splash/view/splash_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../view/login_page.dart' show LoginPage;
+import '../../view/login_page.dart';
 import '../bloc/splash_bloc.dart';
-// Navigate to Login
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -19,16 +18,68 @@ class SplashScreen extends StatelessWidget {
             );
           }
         },
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body: Center(
-            child: SizedBox(
-              height: 100,
-              width: 230,
-              child: Image.asset("assets/images/Logo (1).png"),
-            ),
-          ),
-        ),
+        child: const _SplashView(),
+      ),
+    );
+  }
+}
+
+/// Extracted splash view
+class _SplashView extends StatelessWidget {
+  const _SplashView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: const Center(
+        child: _SplashLogo(),
+      ),
+    );
+  }
+}
+
+/// Splash logo with fade-in animation
+class _SplashLogo extends StatefulWidget {
+  const _SplashLogo();
+
+  @override
+  State<_SplashLogo> createState() => _SplashLogoState();
+}
+
+class _SplashLogoState extends State<_SplashLogo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..forward();
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _fadeAnimation,
+      child: SizedBox(
+        height: 100,
+        width: 230,
+        child: Image.asset("assets/images/Logo (1).png"),
       ),
     );
   }
